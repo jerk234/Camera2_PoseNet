1fff1c930a209f9312c48068285d9296f5548633
jerkAbr <109883400+jerkAbr@users.noreply.github.com>
2024-04-11 12:27:49 -0400

    Initial commit

---
+++ app/src/main/java/com/programminghut/pose_detection/MainActivity.kt
@@ -0,0 +1,142 @@
+package com.programminghut.pose_detection
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.*
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import android.view.Surface
+import android.view.TextureView
+import android.widget.ImageView
+import com.programminghut.pose_detection.ml.LiteModelMovenetSingleposeLightningTfliteFloat164
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+
+class MainActivity : AppCompatActivity() {
+
+    val paint = Paint()
+    lateinit var imageProcessor: ImageProcessor
+    lateinit var model: LiteModelMovenetSingleposeLightningTfliteFloat164
+    lateinit var bitmap: Bitmap
+    lateinit var imageView: ImageView
+    lateinit var handler: Handler
+    lateinit var handlerThread: HandlerThread
+    lateinit var textureView: TextureView
+    lateinit var cameraManager: CameraManager
+    val points: MutableList<Pair<Float, Float>> = mutableListOf() // Store screen coordinates of points
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        getPermissions()
+
+        imageProcessor = ImageProcessor.Builder().add(ResizeOp(192, 192, ResizeOp.ResizeMethod.BILINEAR)).build()
+        model = LiteModelMovenetSingleposeLightningTfliteFloat164.newInstance(this)
+        imageView = findViewById(R.id.imageView)
+        textureView = findViewById(R.id.textureView)
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
+
+        paint.color = Color.YELLOW
+        paint.textSize = 40f // Set text size for coordinates
+
+        textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
+                openCamera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {}
+
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean { return false }
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+                bitmap = textureView.bitmap!!
+                var tensorImage = TensorImage(DataType.UINT8)
+                tensorImage.load(bitmap)
+                tensorImage = imageProcessor.process(tensorImage)
+
+                val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 192, 192, 3), DataType.UINT8)
+                inputFeature0.loadBuffer(tensorImage.buffer)
+
+                val outputs = model.process(inputFeature0)
+                val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
+
+                points.clear() // Clear previous points
+
+                var h = bitmap.height
+                var w = bitmap.width
+                var x = 0
+                var featureIndex = 0
+
+                val mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                val canvas = Canvas(mutable) // Create canvas here
+
+                while (x <= 49) {
+                    val confidence = outputFeature0.get(x + 2)
+                    if (confidence > 0.45) {
+                        val pointX = outputFeature0.get(x + 1) * w
+                        val pointY = outputFeature0.get(x) * h
+                        points.add(pointX to pointY) // Store screen coordinates
+
+                        // Draw point and label only if confidence is high enough
+                        canvas.drawCircle(pointX, pointY, 10f, paint)
+                        canvas.drawText("($pointX, $pointY)", pointX, pointY, paint) // Draw coordinates
+                        canvas.drawText("$featureIndex", pointX + 20, pointY - 20, paint) // Draw feature index
+                        featureIndex++
+                    }
+                    x += 3
+                }
+
+                imageView.setImageBitmap(mutable)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        model.close()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun openCamera() {
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object : CameraDevice.StateCallback() {
+            override fun onOpened(p0: CameraDevice) {
+                val captureRequest = p0.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                val surface = Surface(textureView.surfaceTexture)
+                captureRequest.addTarget(surface)
+                p0.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(p0: CameraCaptureSession) {
+                        p0.setRepeatingRequest(captureRequest.build(), null, null)
+                    }
+                    override fun onConfigureFailed(p0: CameraCaptureSession) {}
+                }, handler)
+            }
+            override fun onDisconnected(p0: CameraDevice) {}
+            override fun onError(p0: CameraDevice, p1: Int) {}
+        }, handler)
+    }
+
+    private fun getPermissions() {
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) getPermissions()
+    }
+}
