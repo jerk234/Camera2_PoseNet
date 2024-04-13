7103c2dcb1b508bcc1c57500e1bc0f0ff2b4782d
xp <huangzishuo886@gmail.com>
2024-04-13 19:36:42 +0800

    Commit in CHINA 19:33PM  IN Apr13

---
+++ app/src/main/java/com/programminghut/pose_detection/BodyPartRanges.kt
@@ -0,0 +1,26 @@
+package com.programminghut.pose_detection
+
+object BodyPartRanges {
+    val ranges = mapOf(
+        0 to Pair(265f to 800f, 825f to 1168f),
+        1 to Pair(235f to 784f, 794f to 1290f),
+        2 to Pair(235f to 784f, 794f to 1290f),
+        3 to Pair(256f to 750f, 720f to 1285f),
+        4 to Pair(256f to 750f, 720f to 1285f),
+        5 to Pair(343f to 700f, 850f to 1300f),
+        6 to Pair(343f to 700f, 850f to 1300f),
+        7 to Pair(300f to 650f, 900f to 1100f),
+        8 to Pair(800f to 950f, 900f to 1100f),
+        11 to Pair(600f to 700f, 1100f to 1400f),
+        12 to Pair(580f to 700f, 1100f to 1300f),
+        13 to Pair(750f to 850f, 1100f to 1300f),
+        13 to Pair(750f to 950f, 1100f to 1300f),
+        13 to Pair(750f to 950f, 900f to 1100f),
+
+//        0 to Pair(2f to 8f, 8f to 11f),
+//        1 to Pair(2f to 7f, 74f to 129f),
+//        3 to Pair(2f to 7f, 2f to 12f),
+//        5 to Pair(3f to 7f, 8f to 13f),
+//        7 to Pair(3f to 6f, 9f to 11f)
+    )
+}
